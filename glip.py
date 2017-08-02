e9d6e143161f4809b4061f3720bf466009a25185
Tomáš Houfek <tomas.houfek@eledus.cz>
2017-08-02 14:12:38 +0200

    Trying to get values of element

--- glip.py
+++ glip.py
@@ -14,15 +14,27 @@ from selenium.webdriver.common.action_chains import ActionChains
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-
-
+import time
 
 driver = webdriver.Chrome("../new_hodoor/Hodoor/node_modules/chromedriver/lib/chromedriver/chromedriver")
 driver.get('https://glip.com/')
-driver.find_element_by_id("sign_in").click()
-username = driver.find_element_by_name("email")
-password = driver.find_element_by_name("password")
+
+
+#wait_for_element_to_be_clickable_with_css_selector_click(driver,"//li[@class='navItem signIn']")
+driver.find_element_by_id('sign_in').click()
+time.sleep(1)
+email = driver.find_element_by_name('email')
+password = driver.find_element_by_name('password')
 
 password.send_keys("MoT159357")
-username.send_keys("tomas.houfek@eledus.cz")
+email.send_keys("tomas.houfek@eledus.cz")
 driver.find_element_by_class_name("submit").click()
+time.sleep(20)
+groups = driver.find_elements_by_class_name("grouptab")
+texts = driver.find_elements_by_class_name("post")
+for group in groups:
+    group_name = group.get_attribute('innerHTML')
+    print(group_name)
+for text in texts:
+    text_value = group.get_attribute('innerHTML')
+    print(text_value)
